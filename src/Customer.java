dcb1dfe1a2322828e48ddbaf91fed29f6ea24c07
Administrator <468679810@qq.com>
2021-01-11 15:12:56 +0800

    Initial commit

---
+++ src/Customer.java
@@ -0,0 +1,38 @@
+public class Customer {
+    private String id;
+    private String name;
+    private String idCard;
+    private String phone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
