b2b0764bb10d6e031825f1bbdf3d2321eb39be6c
jinzhao-wei <468679810@qq.com>
2021-01-12 15:03:12 +0800

    添加座位信息

--- src/Message/Flight.java
+++ src/Message/Flight.java
@@ -8,6 +8,8 @@ public class Flight {
     private String departureCity;//出发地
     private String destnation;//目的地
     private Date date;//出发日期
+    private String Seats;//座位
+    private String totalSeats;//总座位
 
     public String getId() {
         return id;
@@ -39,5 +41,29 @@ public class Flight {
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public String getPlanType() {
+        return planType;
+    }
+
+    public void setPlanType(String planType) {
+        this.planType = planType;
+    }
+
+    public String getSeats() {
+        return Seats;
+    }
+
+    public void setSeats(String seats) {
+        Seats = seats;
+    }
+
+    public String getTotalSeats() {
+        return totalSeats;
+    }
+
+    public void setTotalSeats(String totalSeats) {
+        this.totalSeats = totalSeats;
     }
 }
